272ac6ca443c8fe61c0427bb6d813fd62937c51d
WizardAlex77 <65289433+WizardAlex77@users.noreply.github.com>
2020-05-30 21:46:41 +0800

    debugging atob error

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
 import SignUpContainer from './container/SignUpContainer';
 import UserListContainer from "./container/UserList";
+import {decode, encode} from 'base-64'
+
+if (!global.btoa) {  global.btoa = encode }
+
+if (!global.atob) { global.atob = decode }
 
 export default function App() {
   return (
